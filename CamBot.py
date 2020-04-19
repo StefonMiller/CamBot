f547efe215c9e11d2c24ba4bc312c9ea5eb38825
Stefon Miller <35376880+StefonMiller@users.noreply.github.com>
2020-04-19 17:57:07 -0400

    Bug fixes
    Fixed bugs with crafting calculator when entering no name or negative values. Implemented server searching feature to check pop of any server. Migrated SQL connection info to an external text file. Implemented tweeting images. Fixed some formatting

--- CamBot.py
+++ CamBot.py
@@ -12,24 +12,28 @@ from datetime import datetime
 with open('C:/Users/Stefon/PycharmProjects/CamBot/keys.txt') as f:
     keys = f.read().splitlines()
 
+with open('C:/Users/Stefon/PycharmProjects/CamBot/serverinfo.txt') as f:
+    server_info = f.read().splitlines()
+
 client = discord.Client()
 try:
-    c = mysql.connector.connect(host='rustitems.cfo3xmszwucs.us-east-2.rds.amazonaws.com', database='rustitems',
-                                user='root', password='Stefonmiller1')
+    c = mysql.connector.connect(host=server_info[0], database=server_info[1],
+                                user=server_info[2], password=server_info[3])
     cursor = c.cursor()
     print('Connected to server')
 except mysql.connector.Error as e:
     print('Failed to connect to server'.format(e))
 
 
-def listCommands():
+def list_commands():
     return ('Here is a list of commands:\n'
             '\t**!craftcalc** outputs the recipe of a certain item\n'
             '\t**!status** gives you the current status of Cambot\'s dependent servers\n'
-            '\t**!serverpop gives the current pop for Rustafied Trio')
+            '\t**!serverpop** gives the current pop for our frequented servers. Use !serverpop [servername] to get '
+            'information about another server\n')
 
 
-def serverPop(url):
+def server_pop(url):
     try:
         with closing(get(url, stream=True)) as resp:
             content_type = resp.headers['Content-Type'].lower()
@@ -41,18 +45,17 @@ def serverPop(url):
             else:
                 return 'Server not found'
 
-
-    except RequestException as e:
+    except RequestException:
         return 'Connection to Battlemetrics failed'
 
 
-def getStatus():
+def get_status():
     import requests
     p = requests.head('https://www.battlemetrics.com/')
     return p.status_code == 200
 
 
-def craftCalc(itemName, numCrafts):
+def craft_calc(itemName, numCrafts):
     cost = ""
     sql_select_query = """SELECT * FROM craft_recipe WHERE fk_item_name = %s"""
     cursor.execute(sql_select_query, (itemName,))
@@ -60,12 +63,16 @@ def craftCalc(itemName, numCrafts):
     for row in record:
         numcost = int(numCrafts) * row[3]
         if row[1] is None:
-            cost = cost + " **" + row[2] + "**:\t" + str(f"{numcost:,}") + "\n"
+            cost = cost + ' **' + row[2] + '**:\t' + str(f"{numcost:,}") + '\n'
         elif row[2] is None:
-            cost = cost + " **" + row[1] + "**\t" + str(f"{numcost:,}") + "\n"
-    return cost
+            cost = cost + ' **' + row[1] + '**\t' + str(f"{numcost:,}") + '\n'
+    if not record:
+        return 'Item not found in database'
+    else:
+        return 'Crafting cost for ' + str(numCrafts) + ' ' + itemName + ':\n' + cost
 
-def tweet(msg):
+
+def tweet(msg, pic):
     auth = tweepy.OAuthHandler(keys[1], keys[2])
     auth.set_access_token(keys[3], keys[4])
 
@@ -73,11 +80,16 @@ def tweet(msg):
 
     try:
         api.verify_credentials()
-        print("Authentication OK")
+        print('Authentication OK')
     except:
-        print("Error during authentication")
+        print('Error during authentication')
+    if pic is None:
+        api.update_status(msg)
+    else:
+        media = api.media_upload(pic)
+        tweet = msg
+        post_result = api.update_status(status=tweet, media_ids=[media.media_id])
 
-    api.update_status(msg)
 
 @client.event
 async def on_ready():
@@ -91,25 +103,53 @@ async def on_message(message):
 
     # TODO Finish command list
     if message.content.lower().startswith('!cambot help'):
-        await message.channel.send(listCommands())
+        await message.channel.send(list_commands())
 
     # DONE
     elif message.content.lower().startswith('!serverpop'):
-        await message.channel.send('Rustafied Trio currently has ' + serverPop(
-            'https://www.battlemetrics.com/servers/rust/2634280') + ' players online\n'
-                                                                    'Bloo Lagoon currently has ' + serverPop(
-            'https://www.battlemetrics.com/servers/rust/3461363') + ' players online')
+        args = message.content.lower().split()
+        if len(args) == 1:
+            await message.channel.send('Rustafied Trio currently has ' + server_pop(
+                'https://www.battlemetrics.com/servers/rust/2634280') + ' players online\n'
+                                                                        'Bloo Lagoon currently has ' + server_pop(
+                'https://www.battlemetrics.com/servers/rust/3461363') + ' players online')
+        else:
+            serv_name = ""
+            for i in args:
+                if i == args[0]:
+                    pass
+                else:
+                    if serv_name == "":
+                        serv_name = i.capitalize()
+                    else:
+                        serv_name = serv_name + " " + i.capitalize()
+            url = 'https://battlemetrics.com/servers/rust?q=' + serv_name + '&sort=rank'
+            try:
+                with closing(get(url, stream=True)) as resp:
+                    content_type = resp.headers['Content-Type'].lower()
+                    if resp.status_code == 200 and content_type is not None and content_type.find('html') > -1:
+                        html = BeautifulSoup(resp.content, 'html.parser')
+                        server_table = html.find('table', {"class": "css-1yjs8zt"})
+                        entries = server_table.find('tbody')
+                        first_match = entries.find('tr')
+                        link = first_match.find('a').get('href')
+                        serv_name = first_match.find('a').get('title')
+                        url = 'https://battlemetrics.com' + link
+                        await message.channel.send(serv_name + ' currently has ' + server_pop(url) + ' players online')
+                    else:
+                        print('Server not found')
+
+            except RequestException as e:
+                print('Connection to Battlemetrics failed' + e)
 
     elif message.content.lower().startswith('!craftcalc'):
         craftname = ""
         args = message.content.lower().split()
         if len(args) == 1:
-            await message.channel.send("Please enter an item name")
+            await message.channel.send('Please enter an item name')
         else:
             for i in args:
-                if i == args[-1]:
-                    craftnum = i
-                elif i == args[0]:
+                if i == args[0] or i == args[-1]:
                     pass
                 else:
                     if craftname == "":
@@ -117,24 +157,28 @@ async def on_message(message):
                     else:
                         craftname = craftname + " " + i.capitalize()
             try:
-                int(args[-1])
-                await message.channel.send(
-                    "Crafting cost for " + craftnum + " " + craftname + ":\n" + craftCalc(craftname, craftnum))
+                args[-1] = int(args[-1])
+                if args[-1] <= 0:
+                    print('Please enter a valid number')
+                else:
+                    craftnum = args[-1]
+                    await message.channel.send(craft_calc(craftname, craftnum))
             except Exception as e:
                 if craftname == "":
                     craftname = i.capitalize()
                 else:
                     craftname = craftname + " " + i.capitalize()
-                await message.channel.send("Crafting cost for 1 " + craftname + ":\n" + craftCalc(craftname, 1))
+                await message.channel.send(craft_calc(craftname, 1))
 
     elif message.content.lower().startswith('!tweet'):
-        msg = 'HELLO IT IS ME FIRST WORDS ON HERE'
-        tweet(msg)
-        await message.channel.send("New tweet created at " + datetime.now().strftime("%m-%d-%Y %H:%M:%S") + "EST")
+        msg = '@yvngalec @AidanT5 MMM SHOOT PEEPLE WIN GAME ME WILL BE ON CHATURBATE'
+        pic = 'C:/Users/Stefon/PycharmProjects/CamBot/delete.jpg'
+        tweet(msg, pic)
+        await message.channel.send('New tweet created at ' + datetime.now().strftime("%m-%d-%Y %H:%M:%S") + ' EST')
 
     # DONE
     elif message.content.lower().startswith('!status'):
-        if getStatus():
+        if get_status():
             await message.channel.send('All servs r hot n ready like little C\'s')
         else:
             await message.channel.send('Servs are NOT hot!!!')
